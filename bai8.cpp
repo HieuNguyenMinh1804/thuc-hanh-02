703e35cc6921bcbb4ad7d5a6a83b535839b14dc3
HieuNM <154902053+HieuNguyenMinh1804@users.noreply.github.com>
2024-04-13 15:45:37 +0700

    Add files via upload

---
+++ bai8.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+int a[2001][2001], used[2001][2001];
+int n, m, x, y, z, t, ans;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+struct num{
+	int x, y, dem;
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	int test; cin >> test;
+	while(test--){
+		memset(used, 0, sizeof(used));
+		cin >> n >> m >> x >> y >> z >> t;
+		for(int i = 0; i < n; ++i){
+			for(int j = 0; j < m; ++j) cin >> a[i][j];
+		}
+		if(a[z][t] == 0 || a[x][y] == 0){
+			cout << -1 << endl;
+			continue;
+		}
+		queue <num> q;
+		q.push({x, y, 0});
+		used[x][y] = 1;
+		while(q.size()){
+			num temp = q.front(); q.pop();
+			if(temp.x == z && temp.y == t){
+				cout << temp.dem << endl;
+				break;
+			}	
+			for(int k = 0; k < 4; ++k){		
+				int i1 = temp.x + dx[k];
+				int j1 = temp.y + dy[k];
+				if(i1 >= 0 && i1 < n && j1 >= 0 && j1 < m && a[i1][j1] == 1 && used[i1][j1] == 0){
+					used[i1][j1] = 1;
+					q.push({i1, j1, temp.dem + 1});
+				}
+			}
+		}
+    }
+}
