a4910e9a8ad3b8092f7f1983c7f932b55756e581
HieuNM <154902053+HieuNguyenMinh1804@users.noreply.github.com>
2024-04-13 15:50:35 +0700

    Add files via upload

--- bai8.cpp
+++ bai8.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int a[2001][2001], used[2001][2001];
-int n, m, x, y, z, t, ans;
+int n, m, x, y, z, t;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int dx[4] = {-1, 0, 0, 1};
+int dy[4] = {0, -1, 1, 0};
 
 struct num{
 	int x, y, dem;
@@ -17,6 +17,7 @@ int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 	int test; cin >> test;
 	while(test--){
+		int ok = 1;
 		memset(used, 0, sizeof(used));
 		cin >> n >> m >> x >> y >> z >> t;
 		for(int i = 0; i < n; ++i){
@@ -33,6 +34,7 @@ int main(){
 			num temp = q.front(); q.pop();
 			if(temp.x == z && temp.y == t){
 				cout << temp.dem << endl;
+				ok = 0;
 				break;
 			}	
 			for(int k = 0; k < 4; ++k){		
@@ -44,5 +46,6 @@ int main(){
 				}
 			}
 		}
+		if(ok) cout << -1 << endl;
     }
 }
